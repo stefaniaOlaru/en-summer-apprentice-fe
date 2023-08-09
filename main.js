105cd8d37a3262801590bd8063c8442ad3789745
stefaniaOlaru <stefaniaolaru11@gmail.com>
2023-08-09 16:01:15 +0300

    implemented get for events

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function navigateTo(url) {
 function getHomePageTemplate() {
   return `
    <div id="content">
-      <img class="Ana" src="./src/assets/TMS.png" alt="summer">
+    
       <div class="events flex items-center justify-center flex-wrap">
       </div>
     </div>
@@ -63,40 +63,21 @@ function setupInitialPage() {
 function renderHomePage() {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getHomePageTemplate();
-  // Sample hardcoded event data
-  const eventData = {
-    id: 1,
-    description: 'Sample event description.',
-    img: 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-    name: 'Sample Event',
-    ticketCategories: [
-      { id: 1, description: 'General Admission' },
-      { id: 2, description: 'VIP' },
-    ],
-  };
-  // Create the event card element
-  const eventCard = document.createElement('div');
-  eventCard.classList.add('event-card'); 
-  // Create the event content markup
-  const contentMarkup = `
-    <header>
-      <h2 class="event-title text-2xl font-bold">${eventData.name}</h2>
-    </header>
-    <div class="content">
-      <img src="${eventData.img}" alt="${eventData.name}" class="event-image w-full height-200 rounded object-cover mb-4">
-      <p class="description text-gray-700">${eventData.description}</p>
-    </div>
-  `;
-
-  eventCard.innerHTML = contentMarkup;
-  const eventsContainer = document.querySelector('.events');
-  // Append the event card to the events container
-  eventsContainer.appendChild(eventCard);
+  
+  fetchEvents();
 }
 
 function renderOrdersPage(categories) {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getOrdersPageTemplate();
+
+}
+
+async function fetchEvents() {
+  //const response = await fetch('http://localhost:8080/api/allEvents');
+  const response = await fetch('/api/Event/GetAll');
+  const data = await response.json();
+  console.log(data);
 }
 
 // Render content based on URL
